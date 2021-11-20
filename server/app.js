6d2e63d81a9e04e2947b38c81615bd6e9e2dcddf
Olayeni Anifowose <olayeni45@gmail.com>
2021-11-20 09:57:30 +0000

    feat: boilerplate

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import router from './routes/routes.js'
+
+const app = express()
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('combined'))
+app.use(express.json())
+
+app.use('/', router)
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', '*')
+    next()
+})
+
+app.listen(process.env.SERVER_PORT, () =>
+    console.log(`Server listening on port ${process.env.SERVER_PORT}`)
+)
